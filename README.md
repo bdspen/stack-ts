2e45baa055e16aac3fa89ed139d8849d960c3293
Ben Spenard <bdspen@gmail.com>
2017-10-15 20:03:09 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,29 @@
+<!-- # stack-ts
+
+>A stack data structure written in [typescript](https://www.typescriptlang.org/)
+
+## Usage
+
+To install the package run:
+
+```bash
+npm install stack-ts
+```
+Then, to instantiate a new stack in your project:
+```js
+const Stack = require('stack-ts').Stack;
+const stack = new Stack();
+```
+
+### To Run Tests
+```bash
+npm test
+```
+
+## Methods
+
+### Push
+Add an item to the top of the stack.
+```js
+stack.push({ property: "value" });
+``` -->
--- test/stack.test.js
+++ test/stack.test.js
@@ -24,4 +24,25 @@ describe('Stack', () => {
 
     });
 
+    describe('Stack.push() into empty stack', () => {
+        
+        const stack = new Stack();
+        const data = { prop: "value" };
+        stack.push(data);
+
+        it('should push initial data into the stack and increase list length to 1.', () => {
+            assert.equal(stack.size, 1);
+        });
+
+        it('stack.top should be the correct data', () => {
+            assert.equal(stack.top.data, data);
+        });
+
+        it('stack.prev and list.next should be undefined', () => {
+            assert.equal(stack.next, undefined);
+            assert.equal(stack.prev, undefined);            
+        });
+
+    });
+
 });
